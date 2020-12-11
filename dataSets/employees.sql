b39e5b1950e83d9d20650b09a57b450b972c763b
Denny da Vjncj <daVjncjCode@gmail.com>
2020-12-10 22:16:35 -0500

    updates to model

--- dataSets/employees.sql
+++ dataSets/employees.sql
@@ -3,19 +3,19 @@ CREATE DATABASE daVjncjCorp_data;
 USE daVjncjCorp_data;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     departmentName VARCHAR(25) NOT NULL
     PRIMARY KEY (id)
 );
 CREATE TABLE jobFunction (
-    id INT,
+    id INT AUTO_INCREMENT NOT NULL,
     posTitle VARCHAR(25),
     salary DECIMAL(10,5),
     departmentID INT,
     PRIMARY KEY (id)
 );
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     firstName VARCHAR(20) NOT NULL,
     lastName VARCHAR(20) NOT NULL,
     posID INT NOT NULL,
